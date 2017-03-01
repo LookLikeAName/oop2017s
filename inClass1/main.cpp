e323d901851c277fe6bbad19f1e08bee0e9eb2b0
yccheng66 <yccheng66@gmail.com>
2017-03-01 15:27:42 +0800

    innner product, test cases - exceptional 0301

--- inClass1/main.cpp
+++ inClass1/main.cpp
@@ -1,7 +1,8 @@
 #include <gtest/gtest.h>
-//#include <iostream>
+#include <iostream>
+#include <string>
 
-//using namespace std;
+using namespace std;
 int X(int num1, int num2);
 /*
 int main()
@@ -41,6 +42,17 @@ int innerProduct(int dim,int a[], int b[]) {
     return result;
 }
 
+int innerProduct(int d1,int d2, int a[], int b[]) {
+    int result= 0;
+    if(d1!=d2){
+        throw string("wrong dim");
+    }
+    for(int x=0;x<d1;x++){
+        result+=a[x]*b[x];
+    }
+    return result;
+}
+
 TEST (testInnerProduct, Normal) {
     int a[2] = {0, 1}, b[2] = {1, 1};
 
@@ -56,5 +68,20 @@ TEST (testInnerProduct, Normal1) {
 TEST (testInnerProduct, Normal2) {
     int a[4] = {0, 1, 1, 1}, b[4] = {1, 1, 1, 1};
 
-    ASSERT_EQ(3 ,innerProduct(4, a, b));
+    ASSERT_EQ(3 ,innerProduct(4 , a, b));
 }
+
+TEST (testInnerProduct, Abnormal1) {
+    int a[3] = {0, 1, 1}, b[4] = {1, 1, 1, 1};
+
+    try {
+        // if this fails, innerProduct is not doing its job
+        ASSERT_ANY_THROW(innerProduct(3 ,4, a, b));
+    } catch(string s) {
+        // if this fails, innerProduct does not throw the expected exception
+        ASSERT_EQ(string("wrong dim"),s);
+    }
+}
+
+
+
