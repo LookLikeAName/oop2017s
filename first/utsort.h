28223e1c55f3bed7b8f6392389d79d74a1513106
yccheng66 <yccheng66@gmail.com>
2017-06-12 17:17:22 +0800

    bubble-iterators-containers 0612

--- first/utsort.h
+++ first/utsort.h
@@ -1,6 +1,7 @@
 #ifndef UTSORT_H_INCLUDED
 #define UTSORT_H_INCLUDED
-
+#include <list>
+#include <algorithm>
 template <class Type>
 void swap (Type &a, Type &b){
     Type temp;
@@ -39,6 +40,8 @@ void bubble (Type a[], int s, Comp comp){
         }
     }
 }
+
+
 
 bool lessThan(int a, int b){
     return a < b;
@@ -131,5 +134,83 @@ TEST (Bubble, DecreasingByDistanceToNLambda){
     ASSERT_EQ(2, a[2]);
     ASSERT_EQ(6, a[3]);
     ASSERT_EQ(5, a[4]);
+}
+
+TEST(sort, normalArray){
+    int a[5]={5,2,43,6,9};
+    std::sort(a,a+5);
+
+    ASSERT_EQ(2, a[0]);
+    ASSERT_EQ(5, a[1]);
+    ASSERT_EQ(6, a[2]);
+    ASSERT_EQ(9, a[3]);
+    ASSERT_EQ(43, a[4]);
+
+}
+
+TEST(sort, vector){
+    std::vector<int> a;
+    a.push_back(5);
+    a.push_back(2);
+    a.push_back(43);
+    a.push_back(6);
+    a.push_back(9);
+
+    std::sort(a.begin(),a.end());
+
+    ASSERT_EQ(2, a[0]);
+    ASSERT_EQ(5, a[1]);
+    ASSERT_EQ(6, a[2]);
+    ASSERT_EQ(9, a[3]);
+    ASSERT_EQ(43, a[4]);
+
+}
+
+template <class Iter, class Comp>
+void bubble2 (Iter first, Iter last, Comp comp){
+    for(Iter j = first; j < last; j++){
+        for (Iter i = last-1; i>j; i--){
+            if (comp(*i,*(i-1)))
+                swap(*i,*(i-1));
+        }
+    }
+}
+
+
+TEST(bubble2, normalArray){
+    int a[5]={5,2,43,6,9};
+    bubble2(a,a+5, [](int a, int b){return a<b;});
+
+    ASSERT_EQ(2, a[0]);
+    ASSERT_EQ(5, a[1]);
+    ASSERT_EQ(6, a[2]);
+    ASSERT_EQ(9, a[3]);
+    ASSERT_EQ(43, a[4]);
+
+}
+
+TEST(bubble2, vector){
+    int b[5]={5,2,43,6,9};
+    std::vector<int> a(b,b+5);
+
+    bubble2(a.begin(),a.end(), [](int a, int b){return a<b;});
+
+    ASSERT_EQ(2, a[0]);
+    ASSERT_EQ(5, a[1]);
+    ASSERT_EQ(6, a[2]);
+    ASSERT_EQ(9, a[3]);
+    ASSERT_EQ(43, a[4]);
+
+}
+TEST(bubble2, list){
+    int b[5]={5,2,43,6,9};
+    std::list<int> a(b,b+5);
+
+    /* cannot sort list because list creates bidirectional iterators,
+    not RandomAccessIterators!
+
+    bubble2(a.begin(),a.end(), [](int a, int b){return a<b;});
+
+    */
 }
 #endif // UTSORT_H_INCLUDED
